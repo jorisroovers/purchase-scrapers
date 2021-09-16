0b6072197f6dd4f96d5e39170654d23e2c3a19c2
Joris Roovers <joris.roovers@gmail.com>
2021-09-16 14:25:59 +0200

    Amazon scraper improvements

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # purchase-scrapers
 Personal scripts to scrape online webshop order overviews.
-
 # Amazon
+
+This script assumes you're logged into Amazon. On first run, you'll need to login, close the browser and rerun the script.
 
 ```sh
 export SCRAPER_DATA_DIR="/Users/jroovers/Documents/tmp/playwright-datadir"
+mkdir $SCRAPER_DATA_DIR
 export ORDER_ID="ALL" # optionally set and order id to just fetch details of a single order
 node amazon/scraper.js
 ```
--- amazon/scraper.js
+++ amazon/scraper.js
@@ -1,5 +1,6 @@
 const playwright = require('playwright');
 const fs = require('fs');
+const { exit } = require('process');
 
 // https://stackoverflow.com/a/7228322/381010 
 function randomIntFromInterval(min, max) { // min and max included 
@@ -34,8 +35,8 @@ async function main() {
         await page.goto(`${accountURL}/order-details/ref=ppx_yo_dt_b_order_details_o00?ie=UTF8&orderID=${orderId}`);
         let dateEls = await page.$$(".order-date-invoice-item");
         let date = await dateEls[0].innerText();
-        date = date.replace("Ordered on ", "");
-        console.log("DATE", date);
+        date = date.replace("Ordered on ", "").trim();
+        // console.log("DATE", date);
 
         // shipments on page
         const priceElements = await page.$$(".shipment .a-color-price");
@@ -46,30 +47,67 @@ async function main() {
             let descriptionEl = await priceElement.$('xpath=../..');
             descriptionEl = await descriptionEl.$(".a-row");
             let description = await descriptionEl.innerText();
-            console.log(date, description, price);
+            console.log(date + ";" + description + ";" + price);
 
         });
     }
 
 
     if (orderId === "ALL") {
-        await page.goto(`${accountURL}/order-history?opt=ab&digitalOrders=1&unifiedOrders=1&orderFilter=year-2021`);
 
-        // find orders on page
-        const orderIdElements = await page.$$('.order-info .a-col-right .value');
-        orderId = (await orderIdElements[0].innerText()).trim();
+        // 1. Find all order ids by loading all the order pages and extracting the ids
+        let startIndex = 0;
+        let endIndex = 1000;
+        const orderIds = [];
 
-        orderIdElements.forEach(async element => {
-            const orderId = await element.innerText();
-            console.log(orderId);
+        while (startIndex <= endIndex) {
+            await page.goto(`${accountURL}/order-history?opt=ab&digitalOrders=1&unifiedOrders=1&orderFilter=year-2021&startIndex=${startIndex}`);
 
-            let timeoutLength = 1000 + randomIntFromInterval(100, 2000);
-            console.log("TIMEOUT LENGTH", timeoutLength);
-            await sleep(timeoutLength);
-            
-            // TODO: fix this, timing out?
-            // await fetchOrderDetails(orderId);
-        });
+            if (endIndex == 1000) {
+                const numOrderEl = await page.$$('.num-orders');
+                const numOrders = parseInt((await numOrderEl[0].innerText()).replace(" orders").trim());
+                endIndex = Math.floor(numOrders / 10) * 10;
+                console.log("NUM ORDERS", numOrders);
+                console.log("END INDEX", endIndex);
+            }
+
+            // find orders on page
+            const orderIdElements = await page.$$('.order-info .a-col-right .value');
+            orderId = (await orderIdElements[0].innerText()).trim(); // ensure we can actually read order Ids
+
+            console.log("ORDERS EL LENGTH", orderIdElements.length);
+            // Extract ids from orders
+            for (const orderEl of orderIdElements) {
+                const orderId = await orderEl.innerText();
+                orderIds.push(orderId);
+                console.log("ORDER ID FOUND", orderId);
+            }
+            startIndex += 10;
+        }
+
+
+        console.log("TOTAL ORDERS FOUND", orderIds.length);
+        console.log("--------------------");
+
+        // 2. For each order id found, load the order details, wait some random time in between page loads
+
+        processOrders = async function (i) {
+            if (i < orderIds.length) {
+                const orderId = orderIds[i];
+
+                let timeoutLength = 1000 + randomIntFromInterval(100, 5000);
+                await sleep(timeoutLength);
+                console.log("FETCHING DETAILS FOR", orderId);
+
+                try {
+                    await fetchOrderDetails(orderId);
+                } catch (e) {
+                    console.log(e);
+                }
+                processOrders(i + 1)
+            }
+        }
+        processOrders(0)
 
     } else {
         await fetchOrderDetails(orderId);
